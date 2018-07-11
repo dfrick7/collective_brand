2647e525cdaecb9fccb205450cc281848f36075c
Daniel Frick <danielfrick7@gmail.com>
2018-07-11 17:48:08 -0500

    add Brand Collective Proj

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	document.documentElement.style.opacity = '1';}
+
+(function($) {
+	window.fnames = new Array();
+	window.ftypes = new Array();
+	fnames[1]='NAME';
+	ftypes[1]='text';
+	fnames[0]='EMAIL';
+	ftypes[0]='email';
+	fnames[2]='DRIBBBLE';
+	ftypes[2]='text';
+	fnames[6]='REF';
+	ftypes[6]='text';
+}
+(jQuery));
+var $mcj = jQuery.noConflict(true);
